3c6ec5c6ab49e19c82d462e2e41a98c479c05dc8
Tom <83677440+TomMc608@users.noreply.github.com>
2022-06-14 12:32:48 +1200

    updated code for Column

--- rubik_s_cube/rubik_s_cube.ino
+++ rubik_s_cube/rubik_s_cube.ino
@@ -82,6 +82,25 @@ void loop() {
   Face6Row1();
   Face6Row2();
   Face6Row3();
+
+  Face1Column1():
+  Face1Column2():
+  Face1Column3():
+  Face2Column1():
+  Face2Column2():
+  Face2Column3():
+  Face3Column1():
+  Face3Column2():
+  Face3Column3():
+  Face4Column1():
+  Face4Column2():
+  Face4Column3():
+  Face5Column1():
+  Face5Column2():
+  Face5Column3():
+  Face6Column1():
+  Face6Column2():
+  Face6Column3():
 }
 
 void Face1Row1() {
@@ -225,11 +244,10 @@ void Face6Row2() {
 }
 
 void Face6Row3() {
-  int x = 52; int y = 53; int z = 54;
-  leds[x] = CRGB (128, 0, 0);
-  leds[y] = CRGB (128, 0, 0);
-  leds[z] = CRGB (128, 0, 0);
+  for(int i = 51; i <= 53; i++){
+  leds[i] = CRGB (0, 255, 0)
   FastLED.show();
+  }
 
 }
 
@@ -240,151 +258,151 @@ void Face6Row3() {
 
 
 void Face1Column1() {
-  for (int i = 0; i <= 2; i++) {
-    leds[i] = CRGB (255, 0, 0);
-    FastLED.show();
+  int x = 0; int y = 3; int z = 6;
+  leds[x] = CRGB (255, 0, 0);
+  leds[y] = CRGB (255, 0, 0);
+  leds[z] = CRGB (128, 0, 0);
+  FastLED.show();
 
-  }
+  
 }
 
-void Face1Row2() {
-  for (int i = 3; i <= 5; i++) {
-    leds[i] = CRGB (255, 0, 0);
-    FastLED.show();
-
-  }
+void Face1Column2() {
+  int x = 1; int y = 53; int z = 54;
+  leds[x] = CRGB (255, 0, 0);
+  leds[y] = CRGB (255, 0, 0);
+  leds[z] = CRGB (255, 0, 0);
+  FastLED.show();
 }
 
 void Face1Column3() {
-  for (int i = 6; i <= 8; i++) {
-    leds[i] = CRGB (255, 0, 0);
-    FastLED.show();
-
-  }
+  int x = 2; int y = 53; int z = 54;
+  leds[x] = CRGB (255, 0, 0);
+  leds[y] = CRGB (255, 0, 0);
+  leds[z] = CRGB (255, 0, 0);
+  FastLED.show();
 }
 
 //----------------------------------------
 
 void Face2Column1() {
-  for (int i = 9; i <= 11; i++) {
-    leds[i] = CRGB (255, 255, 255);
-    FastLED.show();
-
-  }
+  int x = 3; int y = 3; int z = 6;
+  leds[x] = CRGB (255, 255, 255);
+  leds[y] = CRGB (255, 255, 255);
+  leds[z] = CRGB (255, 255, 255);
+  FastLED.show();
 }
 
 void Face2Column2() {
-  for (int i = 12; i <= 14; i++) {
-    leds[i] = CRGB (255, 255, 255);
-    FastLED.show();
-
-  }
+ int x = 4; int y = 4; int z = 7;
+  leds[x] = CRGB (255, 255, 255);
+  leds[y] = CRGB (255, 255, 255);
+  leds[z] = CRGB (255, 255, 255);
+  FastLED.show();
 }
 
 void Face2Column3() {
-  for (int i = 15; i <= 17; i++) {
-    leds[i] = CRGB (255, 255, 255);
-    FastLED.show();
-
-  }
+ int x = 5; int y = 5; int z = 8;
+  leds[x] = CRGB (255, 255, 255);
+  leds[y] = CRGB (255, 255, 255);
+  leds[z] = CRGB (255, 255, 255);
+  FastLED.show();
 }
 void Face3Column1() {
-  for (int i = 18; i <= 20; i++) {
-    leds[i] = CRGB (255, 255, 0);
-    FastLED.show();
-
-  }
+ int x = 6; int y = 6; int z = 9;
+  leds[x] = CRGB (255, 255, 0);
+  leds[y] = CRGB (255, 255, 0);
+  leds[z] = CRGB (255, 255, 0);
+  FastLED.show();
 }
 
 void Face3Column2() {
-  for (int i = 21; i <= 23; i++) {
-    leds[i] = CRGB (255, 255, 0);
-    FastLED.show();
-
-  }
+ int x = 7; int y = 53; int z = 54;
+  leds[x] = CRGB (255, 255, 0);
+  leds[y] = CRGB (255, 255, 0);
+  leds[z] = CRGB (255, 255, 0);
+  FastLED.show();
 }
 
 void Face3Column3() {
-  for (int i = 24; i <= 26; i++) {
-    leds[i] = CRGB (255, 255, 0);
-    FastLED.show();
-
-  }
+ int x = 8; int y = 53; int z = 54;
+  leds[x] = CRGB (255, 255, 0);
+  leds[y] = CRGB (255, 255, 0);
+  leds[z] = CRGB (255, 255, 0);
+  FastLED.show();
 }
 
 //----------------------------------------
 void Face4Column1() {
-  for (int i = 27; i <= 29; i++) {
-    leds[i] = CRGB (0, 0, 255);
-    FastLED.show();
-
-  }
+ int x = 9; int y = 53; int z = 54;
+  leds[x] = CRGB (0, 0, 255);
+  leds[y] = CRGB (0, 0, 255);
+  leds[z] = CRGB (0, 0, 255);
+  FastLED.show();
 }
 
 void Face4Column2() {
-  for (int i = 30; i <= 32; i++) {
-    leds[i] = CRGB (0, 0, 255);
-    FastLED.show();
-
-  }
+ int x = 10; int y = 53; int z = 54;
+  leds[x] = CRGB (0, 0, 255);
+  leds[y] = CRGB (0, 0, 255);
+  leds[z] = CRGB (0, 0, 255);
+  FastLED.show();
 }
 
 void Face4Column3() {
-  for (int i = 33; i <= 35; i++) {
-    leds[i] = CRGB (0, 0, 255);
-    FastLED.show();
-
-  }
+ int x = 11; int y = 53; int z = 54;
+  leds[x] = CRGB (0, 0, 255);
+  leds[y] = CRGB (0, 0, 255);
+  leds[z] = CRGB (0, 0, 255);
+  FastLED.show();
 }
 
 //----------------------------------------
 void Face5Column1() {
-  for (int i = 36; i <= 38; i++) {
-    leds[i] = CRGB (255, 165, 0);
-    FastLED.show();
-
-  }
+ int x = 12; int y = 53; int z = 54;
+  leds[x] = CRGB (255, 165, 0);
+  leds[y] = CRGB (255, 165, 0);
+  leds[z] = CRGB (255, 165, 0);
+  FastLED.show();
 }
 
 void Face5Column2() {
-  for (int i = 39; i <= 41; i++) {
-    leds[i] = CRGB (255, 165, 0);
-    FastLED.show();
-
-  }
+ int x = 13; int y = 53; int z = 54;
+  leds[x] = CRGB (255, 165, 0);
+  leds[y] = CRGB (255, 165, 0);
+  leds[z] = CRGB (255, 165, 0);
+  FastLED.show();
 }
 
 void Face5Column3() {
-  for (int i = 42; i <= 44; i++) {
-    leds[i] = CRGB (255, 165, 0);
-    FastLED.show();
-
-  }
+ int x = 14; int y = 53; int z = 54;
+  leds[x] = CRGB (255, 165, 0);
+  leds[y] = CRGB (255, 165, 0);
+  leds[z] = CRGB (255, 165, 0);
+  FastLED.show();
 }
 
 //----------------------------------------
 void Face6Column1() {
-  for (int i = 45; i <= 47; i++) {
-    leds[i] = CRGB (0, 255, 0);
-    FastLED.show();
-
-  }
+  int x = 15; int y = 53; int z = 54;
+  leds[x] = CRGB (0, 255, 0);
+  leds[y] = CRGB (0, 255, 0);
+  leds[z] = CRGB (0, 255, 0);
+  FastLED.show();
 }
 
 void Face6Column2() {
-  for (int i = 48; i <= 50; i++) {
-    leds[i] = CRGB (0, 255, 0);
-    FastLED.show();
-
-  }
+ int x = 13; int y = 53; int z = 54;
+  leds[x] = CRGB (0, 255, 0);
+  leds[y] = CRGB (0, 255, 0);
+  leds[z] = CRGB (0, 255, 0);
+  FastLED.show();
 }
 
 void Face6Column3() {
-  for (int i = 51; i <= 53; i++) {
-    leds[i] = CRGB (0, 255, 0);
-    FastLED.show();
-
-  }
+  int x = 14; int y = 53; int z = 54;
+  leds[x] = CRGB (0, 255, 0);
+  leds[y] = CRGB (0, 255, 0);
+  leds[z] = CRGB (0, 255, 0);
+  FastLED.show();
 }
-
-* /
